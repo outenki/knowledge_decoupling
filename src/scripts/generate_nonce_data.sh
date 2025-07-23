ba9a7348e0e129a388716cf90ab183538d36002b
outenki <outenki@gmail.com>
2025-07-23 19:32:25 +0900

    update script

--- src/scripts/generate_nonce_data.sh
+++ src/scripts/generate_nonce_data.sh
@@ -3,5 +3,4 @@ uv run python /home/pj25000107/ku50001566/projects/knowledge_decoupling/src/gene
     -dp /home/pj25000107/ku50001566/projects/knowledge_decoupling/output/data \
     -dn preprocessed \
     -lf local \
-    -o /home/pj25000107/ku50001566/projects/knowledge_decoupling/output/data/nonce \
-    -l 10000
+    -o /home/pj25000107/ku50001566/projects/knowledge_decoupling/output/data/nonce
