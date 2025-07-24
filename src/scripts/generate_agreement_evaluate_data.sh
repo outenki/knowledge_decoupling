bed39858346b63538f52fd2a60e1d8c970b01835
outenki <outenki@gmail.com>
2025-07-24 15:44:24 +0900

    update script

--- src/scripts/generate_agreement_evaluate_data.sh
+++ src/scripts/generate_agreement_evaluate_data.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-uv run \
-python /home/pj25000107/ku50001566/projects/knowledge_decoupling/src/generate_agreement_evaluate_data.py \
+uv run python /home/pj25000107/ku50001566/projects/knowledge_decoupling/src/generate_agreement_evaluate_data.py \
     -dp /home/pj25000107/ku50001566/projects/knowledge_decoupling/output/data/preprocessed  \
     -dn test \
     -lf local \
