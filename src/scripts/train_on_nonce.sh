f5598c5fada3bafec82da924c9ff02608e9f9871
outenki <outenki@gmail.com>
2025-08-05 13:50:37 +0900

    add jobs

---
+++ src/scripts/train_on_nonce.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+SCRIPT_PATH=/home/pj25000107/ku50001566/projects/knowledge_decoupling/src
+DATA_NAME=nonce
+for size in 10k 100k
+do
+    echo "====== training on nonce_$size ======"
+    uv run python $SCRIPT_PATH/train.py \
+        -dp $SCRIPT_PATH/../input/${DATA_NAME}_$size \
+        -if config \
+        -cn gpt-mini \
+        -o $SCRIPT_PATH/../output/gpt-mini/${DATA_NAME}_$size
+done
