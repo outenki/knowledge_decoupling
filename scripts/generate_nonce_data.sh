78ad81e92a9d2efe2899f592afc841ccaafec78f
outenki <outenki@gmail.com>
2025-08-05 17:40:45 +0900

    update nonce generation script

--- scripts/generate_nonce_data.sh
+++ scripts/generate_nonce_data.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
+BASE_PATH=/home/pj25000107/ku50001566/projects/knowledge_decoupling
 echo "====== wikitext_with_nonce_1000k ======"
-/home/pj25000107/ku50001566/.local/bin/uv run python /home/pj25000107/ku50001566/projects/knowledge_decoupling/src/generate_nonce_data.py \
-    -dp /home/pj25000107/ku50001566/projects/knowledge_decoupling/data/preprocessed \
+/home/pj25000107/ku50001566/.local/bin/uv run python $BASE_PATH/src/generate_nonce_data.py \
+    -dp $BASE_PATH/data/preprocessed_100k \
     -lf local \
     -l 1000000 \
-    -o /home/pj25000107/ku50001566/projects/knowledge_decoupling/data/wikitext_with_nonce_1000k
+    -o $BASE_PATH/data/wikitext_with_nonce_1000k
 
 echo "====== wikitext_with_nonce_500k ======"
-/home/pj25000107/ku50001566/.local/bin/uv run python /home/pj25000107/ku50001566/projects/knowledge_decoupling/src/generate_nonce_data.py \
-    -dp /home/pj25000107/ku50001566/projects/knowledge_decoupling/data/preprocessed \
+/home/pj25000107/ku50001566/.local/bin/uv run python $BASE_PATH/src/generate_nonce_data.py \
+    -dp $BASE_PATH/data/preprocessed_100k \
     -lf local \
     -l 500000 \
-    -o /home/pj25000107/ku50001566/projects/knowledge_decoupling/data/wikitext_with_nonce_500k
+    -o $BASE_PATH/data/wikitext_with_nonce_500k
 
 echo "====== wikitext_with_nonce_100k ======"
-/home/pj25000107/ku50001566/.local/bin/uv run python /home/pj25000107/ku50001566/projects/knowledge_decoupling/src/generate_nonce_data.py \
-    -dp /home/pj25000107/ku50001566/projects/knowledge_decoupling/data/preprocessed \
+/home/pj25000107/ku50001566/.local/bin/uv run python $BASE_PATH/src/generate_nonce_data.py \
+    -dp $BASE_PATH/data/preprocessed_100k \
     -lf local \
     -l 100000 \
-    -o /home/pj25000107/ku50001566/projects/knowledge_decoupling/data/wikitext_with_nonce_100k
+    -o $BASE_PATH/data/wikitext_with_nonce_100k
 
 echo "====== wikitext_with_nonce_50k ======"
-/home/pj25000107/ku50001566/.local/bin/uv run python /home/pj25000107/ku50001566/projects/knowledge_decoupling/src/generate_nonce_data.py \
-    -dp /home/pj25000107/ku50001566/projects/knowledge_decoupling/data/preprocessed \
+/home/pj25000107/ku50001566/.local/bin/uv run python $BASE_PATH/src/generate_nonce_data.py \
+    -dp $BASE_PATH/data/preprocessed_100k \
     -lf local \
     -l 100000 \
-    -o /home/pj25000107/ku50001566/projects/knowledge_decoupling/data/wikitext_with_nonce_50k
+    -o $BASE_PATH/data/wikitext_with_nonce_50k
 
 echo "====== wikitext_with_nonce_10k ======"
-/home/pj25000107/ku50001566/.local/bin/uv run python /home/pj25000107/ku50001566/projects/knowledge_decoupling/src/generate_nonce_data.py \
-    -dp /home/pj25000107/ku50001566/projects/knowledge_decoupling/data/preprocessed \
+/home/pj25000107/ku50001566/.local/bin/uv run python $BASE_PATH/src/generate_nonce_data.py \
+    -dp $BASE_PATH/data/preprocessed_100k \
     -lf local \
     -l 100000 \
-    -o /home/pj25000107/ku50001566/projects/knowledge_decoupling/data/wikitext_with_nonce_10k
+    -o $BASE_PATH/data/wikitext_with_nonce_10k
