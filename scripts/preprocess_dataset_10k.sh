7a62df9e6c5d14271f98c01f06790a4a3d222053
outenki <outenki@gmail.com>
2025-08-05 15:53:02 +0900

    fix output path of preprocess_data

--- scripts/preprocess_dataset_10k.sh
+++ scripts/preprocess_dataset_10k.sh
@@ -2,5 +2,5 @@
 /home/pj25000107/ku50001566/.local/bin/uv run python /home/pj25000107/ku50001566/projects/knowledge_decoupling/src/preprocess_dataset.py \
     -dp wikitext \
     -lf hf \
-    -o data/preprocessed_10k \
+    -o /home/pj25000107/ku50001566/projects/knowledge_decoupling/data/preprocessed_10k \
     -l 10000
