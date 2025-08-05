4819bd90932186cbd09b0367e02dd051d54b0ac4
outenki <outenki@gmail.com>
2025-08-05 15:30:41 +0900

    update script for preprocessing data

--- scripts/preprocess_dataset.sh
+++ scripts/preprocess_dataset.sh
@@ -1,8 +1,7 @@
 
 #!/bin/bash
 uv run python /home/pj25000107/ku50001566/projects/knowledge_decoupling/src/preprocess_dataset.py \
-    -dp wikimedia/wikipedia \
-    -dn 20231101.en \
+    -dp wikitext \
     -lf hf \
-    -o /home/pj25000107/ku50001566/projects/knowledge_decoupling/output/data/preprocessed \
-    -l 10000
+    -o data/preprocessed_test \
+    -l 1000000
