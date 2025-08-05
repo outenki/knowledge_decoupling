6044be913849918ccc6e6f6f52a95e9f9bd0082d
outenki <outenki@gmail.com>
2025-08-05 14:00:24 +0900

    fix typo

--- src/sub_jobs/job_train_on_wikitext.sh
+++ src/sub_jobs/job_train_on_wikitext.sh
@@ -11,4 +11,4 @@ module load singularity-ce
 
 cd /home/pj25000107/ku50001566/projects/knowledge_decoupling/src/scripts || exit 1
 
-singularity exec --nv /home/pj25000107/ku50001566/nlp-singularity/nlp-singularity.sif /bin/bash train_on_wikiext.sh
+singularity exec --nv /home/pj25000107/ku50001566/nlp-singularity/nlp-singularity.sif /bin/bash train_on_wikitext.sh
