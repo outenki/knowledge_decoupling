3819654f73d0f6b3714d623b2023a71d4ac05c9b
outenki <outenki@gmail.com>
2025-08-07 15:56:05 +0900

    add evaluation scripts

--- scripts/agreement_evaluation.sh
+++ scripts/agreement_evaluation.sh
@@ -4,6 +4,7 @@ for model_name in gpt-mini gpt-medium gpt-large;do
     for data_name in wikitext nonce;do
         #           10k   50k   100k   200k   300k   400k   500k
         for size in 10000 50000 100000 200000 300000 400000 500000;do
+            echo "====== Evaluating $model_name trained with $data_name $size ======"
             uv run python agreement_evaluation.py \
                 --model-path $BASE_PATH/output/$model_name/${data_name}_${size} \
                 --model-type pt \
