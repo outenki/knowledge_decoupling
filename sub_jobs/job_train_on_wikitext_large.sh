cb04ec87f1504e173ea794817a4b015d633c24a1
outenki <outenki@gmail.com>
2025-08-07 13:38:02 +0900

    update job logs

--- sub_jobs/job_train_on_wikitext_large.sh
+++ sub_jobs/job_train_on_wikitext_large.sh
@@ -3,12 +3,12 @@
 #PJM -L "elapse=12:00:00"
 #PJM -L "gpu=1"
 #PJM -L "jobenv=singularity"
-#PJM -e /home/pj25000107/ku50001566/projects/knowledge_decoupling/logs/job_train_wikitext.err
-#PJM -o /home/pj25000107/ku50001566/projects/knowledge_decoupling/logs/job_train_wikitext.out
+#PJM -e /home/pj25000107/ku50001566/projects/knowledge_decoupling/logs/job_train_wikitext_gpt_large.err
+#PJM -o /home/pj25000107/ku50001566/projects/knowledge_decoupling/logs/job_train_wikitext_gpt_large.out
 #PJM -N "wikitext_l_train"
 
 module load singularity-ce
 
 cd /home/pj25000107/ku50001566/projects/knowledge_decoupling/scripts || exit 1
 
-singularity exec --nv /home/pj25000107/ku50001566/nlp-singularity/nlp-singularity.sif /bin/bash train_on_wikitext_gpt_medium.sh
+singularity exec --nv /home/pj25000107/ku50001566/nlp-singularity/nlp-singularity.sif /bin/bash train_on_wikitext_gpt_large.sh
--- sub_jobs/job_train_on_wikitext_medium.sh
+++ sub_jobs/job_train_on_wikitext_medium.sh
@@ -3,8 +3,8 @@
 #PJM -L "elapse=12:00:00"
 #PJM -L "gpu=1"
 #PJM -L "jobenv=singularity"
-#PJM -e /home/pj25000107/ku50001566/projects/knowledge_decoupling/logs/job_train_wikitext.err
-#PJM -o /home/pj25000107/ku50001566/projects/knowledge_decoupling/logs/job_train_wikitext.out
+#PJM -e /home/pj25000107/ku50001566/projects/knowledge_decoupling/logs/job_train_wikitext_gpt_medium.err
+#PJM -o /home/pj25000107/ku50001566/projects/knowledge_decoupling/logs/job_train_wikitext_gpt_medium.out
 #PJM -N "wikitext_m_train"
 
 module load singularity-ce
