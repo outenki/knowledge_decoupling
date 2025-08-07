213cd486dcdbda2494201ee07d9971de8f58dc05
outenki <outenki@gmail.com>
2025-08-07 16:08:39 +0900

    update scripts

--- scripts/agreement_evaluation.sh
+++ scripts/agreement_evaluation.sh
@@ -5,7 +5,7 @@ for model_name in gpt-mini gpt-medium gpt-large;do
         #           10k   50k   100k   200k   300k   400k   500k
         for size in 10000 50000 100000 200000 300000 400000 500000;do
             echo "====== Evaluating $model_name trained with $data_name $size ======"
-            uv run python agreement_evaluation.py \
+            uv run python $BASE_PATH/src/agreement_evaluation.py \
                 --model-path $BASE_PATH/output/$model_name/${data_name}_${size} \
                 --model-type pt \
                 --val-data $BASE_PATH/data/evaluate_data/agreement_evaluate_data.json \
