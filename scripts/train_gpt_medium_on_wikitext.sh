39f8c6152f4eca2a9cd95d868dfff19426ec638e
outenki <outenki@gmail.com>
2025-08-07 14:06:52 +0900

    fix scripts

--- scripts/train_gpt_medium_on_wikitext.sh
+++ scripts/train_gpt_medium_on_wikitext.sh
@@ -6,11 +6,11 @@ CONFIG_NAME="gpt-medium"
 #           10k   50k   100k   200k   300k   400k   500k
 for size in 10000 50000 100000 200000 300000 400000 500000
 do
-    echo "====== training on wikitext_$size ======"
+    echo "====== training on ${DATA_NAME}_$size ======"
     /home/pj25000107/ku50001566/.local/bin/uv run python $SCRIPT_PATH/train.py \
         -dp $SCRIPT_PATH/../input/${DATA_NAME} \
-        -if config \
         -dl $size \
+        -if config \
         -cn $CONFIG_NAME \
         -o $SCRIPT_PATH/../output/$CONFIG_NAME/${DATA_NAME}_$size
 done
