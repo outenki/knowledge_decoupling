1f9ded96d9b6f8b1b28559603f66a47b22a7e4b5
outenki <outenki@gmail.com>
2025-09-17 16:26:48 +0900

    refactors

--- jobs/train_large/job_train_gpt_large_on_wikitext_ep3.sh
+++ jobs/train_large/job_train_gpt_large_on_wikitext_ep3.sh
@@ -9,6 +9,6 @@
 
 module load singularity-ce
 
-cd /home/pj25000107/ku50001566/projects/knowledge_decoupling/scripts || exit 1
+cd /home/pj25000107/ku50001566/projects/knowledge_decoupling/scripts/train_large || exit 1
 
 singularity exec --nv /home/pj25000107/ku50001566/nlp-singularity/nlp-singularity.sif /bin/bash train_gpt_large_on_wikitext_ep3.sh
