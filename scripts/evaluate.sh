c92bb8fe54b83b759a98939c05657f1762db2de5
outenki <outenki@gmail.com>
2025-10-07 14:53:22 +0900

    update evaluation

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -10,9 +10,8 @@ model_name=gpt-large
 # eval_name=fce
 # eval_name=qa_boolq
 # eval_name=qa_qasc
-# for eval_name in verb_agreement qa_arc_challenge qa_arc_easy fce qa_boolq qa_boolq_psg qa_qasc; do
-for eval_name in verb_agreement; do
-# for eval_name in qa_boolq_psg; do
+for eval_name in verb_agreement fce fce_3gram qa_arc_easy qa_arc_challenge qa_boolq qa_boolq_psg qa_qasc; do
+# for eval_name in fce_3gram; do
     echo
     echo "============ $eval_name ============"
 
@@ -21,13 +20,13 @@ for eval_name in verb_agreement; do
     #     --model-path gpt2 \
     #     --val-data $BASE_PATH/input/evaluate_data/$eval_name/test.json \
     #     -o $BASE_PATH/output/0830/gpt-large/gpt2/$eval_name
-    # for data_name in init_model wikimedia-bs1024-ep1 wikimedia-nonce-bs1024-ep1 wikimedia-nonce-bs1024-ep3;do
-    for data_name in init_model_0 init_model_1 init_model_2 init_model_3 init_model_4;do
+    for data_name in wikimedia-nonce-bs1024-ep4;do
+    # for data_name in init_model_0 init_model_1 init_model_2 init_model_3 init_model_4;do
         echo
         echo "====== Evaluating $data_name ======"
         python $BASE_PATH/src/evaluate.py \
-            --model-path $BASE_PATH/output/random_models/$model_name/$data_name \
+            --model-path $BASE_PATH/output/1001/$model_name/$data_name \
             --val-data $BASE_PATH/input/evaluate_data/$eval_name/test.json \
-            -o $BASE_PATH/output/random_models/"$model_name"/$data_name/$eval_name
+            -o $BASE_PATH/output/1001/$model_name/$data_name/$eval_name
     done
 done
