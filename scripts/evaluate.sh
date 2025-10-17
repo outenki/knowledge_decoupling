a42270df3bf56a91a9503b93d344af4c84575482
outenki <outenki@gmail.com>
2025-10-17 15:57:48 +0900

    merge pre-generated nonce vocab

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -10,18 +10,18 @@ model_name=gpt-large
 # eval_name=fce
 # eval_name=qa_boolq
 # eval_name=qa_qasc
-for eval_name in verb_agreement fce fce_3gram qa_arc_easy qa_arc_challenge qa_boolq qa_boolq_psg qa_qasc; do
-# for eval_name in fce_3gram; do
+# for eval_name in verb_agreement fce_5gram qa_arc_easy qa_arc_challenge qa_boolq qa_boolq_psg qa_qasc; do
+for eval_name in qa_boolq qa_boolq_psg; do
     echo
     echo "============ $eval_name ============"
 
-    # echo "====== Evaluating hugging face gpt2 ======"
-    # python $BASE_PATH/src/evaluate.py \
-    #     --model-path gpt2 \
-    #     --val-data $BASE_PATH/input/evaluate_data/$eval_name/test.json \
-    #     -o $BASE_PATH/output/0830/gpt-large/gpt2/$eval_name
-    for data_name in wikimedia-nonce-bs1024-ep4;do
-    # for data_name in init_model_0 init_model_1 init_model_2 init_model_3 init_model_4;do
+    echo "====== Evaluating hugging face gpt2 ======"
+    python $BASE_PATH/src/evaluate.py \
+        --model-path gpt2 \
+        --val-data $BASE_PATH/input/evaluate_data/$eval_name/test.json \
+        -o $BASE_PATH/output/0830/gpt-large/gpt2/$eval_name
+
+    for data_name in wikimedia-nonce-bs1024-ep1 wikimedia-nonce-bs1024-ep2 wikimedia-nonce-bs1024-ep3 wikimedia-nonce-bs1024-ep4 wikimedia-nonce-bs1024-ep6 wikimedia-bs1024-dl1_020_000-ep3 simplyfied-wikimedia-bw850-bs1024-ep3 simplyfied-wikimedia-bw850-bs1024-ep10 simplyfied-wikimedia-ox3000-bs1024-ep3 simplyfied-wikimedia-ox3000-bs1024-ep6 wikimedia-bs1024-ep1 wikimedia-bs1024-ep2 wikimedia-bs1024-ep3;do
         echo
         echo "====== Evaluating $data_name ======"
         python $BASE_PATH/src/evaluate.py \
---
+++ src/merge_nonce_vocab.py
@@ -0,0 +1,82 @@
+# merge pre-generated lemma_blacklists and nonce_word_bank.json
+
+from pathlib import Path
+import argparse
+import json
+from tqdm import tqdm
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--path-to-lemma-blacklists', '-lb', dest='path_lemma_blacklists', type=str,
+        help='Path to lemma_blacklists. Files are read in a recursed way.'
+    )
+    parser.add_argument(
+        '--path-to-nonce-word-banks', '-wb', dest='path_word_banks', type=str,
+        help='Path to word_banks. Files are read in a recursed way.'
+    )
+    parser.add_argument(
+        '--output-path', '-o', dest='output_path', required=True, type=str,
+        help='Path to save output. '
+    )
+    return parser.parse_args()
+
+
+def merge_lemma_blacklists(blacklist: set[str], path_to_new_blacklist: str | Path) -> set:
+    with open(path_to_new_blacklist, "r") as f:
+        for line in f:
+            blacklist.add(line.strip())
+    return blacklist
+
+
+def load_word_bank(path_to_word_bank: str | Path) -> dict:
+    with open(path_to_word_bank, "r") as f:
+        word_bank = json.load(f)
+    return {k: set([tuple(t) for t in v]) for k, v in word_bank.items()}
+
+
+def merge_word_bank(word_bank: dict, path_to_new_word_bank: str | Path) -> dict:
+    new_word_bank = load_word_bank(path_to_new_word_bank)
+    for k, v in new_word_bank.items():
+        if k in word_bank:
+            word_bank[k] = word_bank[k] | v
+    return word_bank
+
+
+def main():
+    args = parse_args()
+    path_lemma_blacklists = []
+    path_word_banks = []
+    if args.path_lemma_blacklists:
+        path_lemma_blacklists = list(Path(args.path_lemma_blacklists).rglob("lemma_blacklist"))
+    if args.path_word_banks:
+        path_word_banks = list(Path(args.path_word_banks).rglob("nonce_word_bank.json"))
+
+    output_path = Path(args.output_path)
+    output_path.mkdir(parents=True, exist_ok=True)
+
+    lemma_blacklist = set()
+    output_lemma_blacklist = output_path / "lemma_blacklist"
+    if output_lemma_blacklist.exists():
+        with open(output_lemma_blacklist, "r") as f:
+            lemma_blacklist = set([line.strip() for line in f if line])
+
+    word_bank = {}
+    output_word_bank = output_path / "nonce_word_bank.json"
+    if output_word_bank.exists():
+        word_bank = load_word_bank(output_word_bank)
+
+    for p in tqdm(path_lemma_blacklists, desc="Merging lemma_blacklists"):
+        lemma_blacklist = merge_lemma_blacklists(lemma_blacklist, p)
+    for p in tqdm(path_word_banks, desc="Merging nonce_word_banks"):
+        word_bank = merge_word_bank(word_bank, p)
+
+    if lemma_blacklist:
+        with open(output_lemma_blacklist, "w") as f:
+            for lemma in lemma_blacklist:
+                f.write(f"{lemma}\n")
+
+    if word_bank:
+        with open(output_word_bank, "w") as f:
+            json.dump(word_bank, f, indent=4)
