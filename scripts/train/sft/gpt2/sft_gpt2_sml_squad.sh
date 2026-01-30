2e356be84fb4519948eaf5cb01ca739cdd37a3f5
tianqi.wang <outenki@gmail.com>
2026-01-30 16:39:51 +0900

    update scripts

--- scripts/train/sft/gpt2/sft_gpt2_sml_squad.sh
+++ scripts/train/sft/gpt2/sft_gpt2_sml_squad.sh
@@ -8,14 +8,14 @@ SCRIPT_PATH=$PROJECT_BASE_PATH/scripts/train/sft
 # EPOCHS=$5
 /bin/bash $SCRIPT_PATH/sft.sh \
     gpt2 \
-    $PROJECT_BASE_PATH/output/gpt2/nonce/smolLM2_bs1024_dl0_ep1 \
+    $PROJECT_BASE_PATH/output/gpt2/smolLM2/smolLM2_bs1024_dl0_ep1 \
     $PROJECT_BASE_PATH/input/tokenized/gpt2/sft/squad_v2_ctxt_ans \
-    $PROJECT_BASE_PATH/output/gpt2/nonce/smolLM2_bs1024_dl0_ep1-sft_squad_ans_ep3 \
+    $PROJECT_BASE_PATH/output/gpt2/smolLM2/smolLM2_bs1024_dl0_ep1-sft_squad_ans_ep3 \
     3
 
 /bin/bash $SCRIPT_PATH/sft.sh \
     gpt2 \
-    $PROJECT_BASE_PATH/output/gpt2/nonce/smolLM2_bs1024_dl0_ep1-ext_test_squad_answerable_ep3 \
+    $PROJECT_BASE_PATH/output/gpt2/smolLM2/smolLM2_bs1024_dl0_ep1-ext_test_squad_answerable_ep3 \
     $PROJECT_BASE_PATH/input/tokenized/gpt2/sft/squad_v2_ctxt_ans \
-    $PROJECT_BASE_PATH/output/gpt2/nonce/smolLM2_bs1024_dl0_ep1-ext_test_squad_answerable_ep3-sft_squad_ans_ep3 \
+    $PROJECT_BASE_PATH/output/gpt2/smolLM2/smolLM2_bs1024_dl0_ep1-ext_test_squad_answerable_ep3-sft_squad_ans_ep3 \
     3
