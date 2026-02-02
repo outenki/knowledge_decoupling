f93ecb9685724796784ec6856a8e0ccaac4dcbaa
tianqi.wang <outenki@gmail.com>
2026-02-02 12:06:03 +0900

    update evaluate scripts

--- scripts/evaluate/gpt2/evaluate_on_generation_0shots.sh
+++ scripts/evaluate/gpt2/evaluate_on_generation_0shots.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-BASE_PATH=/Users/ou/projects/knowledge_decoupling
+PROJECT_BASE_PATH="${PROJECT_BASE_PATH:-$HOME/projects/knowledge_decoupling}"
 
 MODEL_NAME=gpt2
 SCORE_ON=generation
@@ -9,19 +9,19 @@ MODE="simple"
 SUFFIX=""
 
 
-for eval_name in unformated/squad_v2_ctxt; do
+for eval_name in unformated/squad_v2_ctxt_answerable; do
     echo
     echo "============ $eval_name ============"
     echo "====== Evaluating Pretrained $MODEL_NAME ======"
     model_path=gpt2
-    uv run python $BASE_PATH/src/evaluate.py \
+    uv run python $PROJECT_BASE_PATH/src/evaluate.py \
         --model $model_path \
         --mode $MODE \
         --tokenizer $MODEL_NAME \
-        --test-data $BASE_PATH/input/evaluate_data/$eval_name/test.json \
+        --test-data $PROJECT_BASE_PATH/input/evaluate_data/$eval_name/test.json \
         --score-on $SCORE_ON \
         --sample-num $SAMPLE_NUM \
-        -o $BASE_PATH/output/$MODEL_NAME/HuggingFace/hf/evaluation$SUFFIX/${SCORE_ON}/${FEWSHOTS}_shots/$eval_name
+        -o $PROJECT_BASE_PATH/output/$MODEL_NAME/HuggingFace/hf/evaluation$SUFFIX/${SCORE_ON}/${FEWSHOTS}_shots/$eval_name
 
     for model_folder in \
         HuggingFace/hf-ext_test_ep3 \
@@ -37,12 +37,12 @@ for eval_name in unformated/squad_v2_ctxt; do
         nonce/smolLM2_nonce_mn3_bs1024_dl0_ep1-ext_test_nonce_ep3-sft_squad_ans_ep3
     do
         echo "====== Evaluating $model_folder of $MODEL_NAME ======"
-        model_path=$BASE_PATH/output/$MODEL_NAME/$model_folder
-        uv run python $BASE_PATH/src/evaluate.py \
+        model_path=$PROJECT_BASE_PATH/output/$MODEL_NAME/$model_folder
+        uv run python $PROJECT_BASE_PATH/src/evaluate.py \
             --model $model_path \
             --mode $MODE \
             --tokenizer $MODEL_NAME \
-            --test-data $BASE_PATH/input/evaluate_data/$eval_name/test.json \
+            --test-data $PROJECT_BASE_PATH/input/evaluate_data/$eval_name/test.json \
             --score-on $SCORE_ON \
             --sample-num $SAMPLE_NUM \
             -o $model_path/evaluation$SUFFIX/${SCORE_ON}/${FEWSHOTS}_shots/$eval_name
