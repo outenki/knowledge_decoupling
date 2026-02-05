45418480667c970e19bea45254283f82ef151414
outenki <outenki@gmail.com>
2026-02-05 17:49:34 +0900

    update: tokenize script could tokenize without slicing

--- src/tokenize_and_slice_data.py
+++ src/tokenize_and_slice_data.py
@@ -1,6 +1,8 @@
 import argparse
 from functools import partial
 from pathlib import Path
+from itertools import chain
+
 from transformers import AutoTokenizer
 
 from lib.dataset import load_custom_dataset
@@ -15,25 +17,40 @@ def read_args():
     parser.add_argument("--data-column", "-dc", type=str, choices=["text", "nonce"], required=True)
     parser.add_argument("--tokenize", "-t", action="store_true")
     parser.add_argument("--slice", "-s", action="store_true")
-    parser.add_argument("--block-size", "-bs", type=int, default=None)
+    parser.add_argument("--block-size", "-bs", type=int, required=True)
     parser.add_argument("--output-path", "-o", type=str, required=True)
     return parser.parse_args()
 
 
-def tokenize_examples(examples, tokenizer, column_name: str):
-    result = tokenizer(
-        examples[column_name],
-        padding=False,
-        truncation=False,
-        return_attention_mask=False,
-    )
-    result["labels"] = [ids[:] for ids in result["input_ids"]]
+def tokenize_examples(examples, tokenizer, column_name: str, padding: bool, max_length: int):
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.padding_side = "right"
+    if padding:
+        result = tokenizer(
+            examples[column_name],
+            padding="max_length",
+            max_length=max_length,
+            truncation=True,
+            return_attention_mask=True,
+        )
+        result["labels"] = [
+            [(l if l != tokenizer.pad_token_id else -100) for l in ids]
+            for ids in result["input_ids"]
+        ]
+    else:
+        result = tokenizer(
+            examples[column_name],
+            padding=False,
+            truncation=False,
+            return_attention_mask=False,
+        )
+        result["labels"] = [ids[:] for ids in result["input_ids"]]
     return result
 
 
 def group_texts_to_blocks(examples, block_size: int):
     # 拼接为一个长序列后再切块
-    concatenated = sum(examples["input_ids"], [])
+    concatenated = list(chain.from_iterable(examples["input_ids"]))
     total_length = (len(concatenated) // block_size) * block_size
     input_blocks = [concatenated[i:i + block_size] for i in range(0, total_length, block_size)]
 
@@ -53,10 +70,18 @@ def main():
     if args.tokenize:
         print(">>> Tokenizing ...")
         tokenizer = AutoTokenizer.from_pretrained(args.tokenizer)
-        tokenizer.pad_token = tokenizer.eos_token
-        tokenizer.padding_side = "left"
 
-        map_func = partial(tokenize_examples, tokenizer=tokenizer, column_name=args.data_column)
+        padding = True
+        if args.slice:
+            padding = False
+
+        map_func = partial(
+            tokenize_examples,
+            tokenizer=tokenizer,
+            column_name=args.data_column,
+            padding=padding,
+            max_length=args.block_size
+        )
         tokenized_datasets = datasets.map(
             map_func,
             batched=True,
@@ -88,7 +113,7 @@ def main():
                 remove_columns=tokenized_datasets["train"].column_names
             )
 
-            bin_path = output_path
+            bin_path = output_path / f"bs_{bs}"
             print(f"  -> Save to: {bin_path}")
             lm_datasets.save_to_disk(str(bin_path))
 
