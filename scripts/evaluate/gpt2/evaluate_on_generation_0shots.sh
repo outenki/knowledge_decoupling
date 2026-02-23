9a966a0b621b355c9fda2d89b5b03a598494e39a
outenki <outenki@gmail.com>
2026-02-23 18:43:45 +0900

    update evaluate scripts

--- scripts/evaluate/gpt2/evaluate_on_generation_0shots.sh
+++ scripts/evaluate/gpt2/evaluate_on_generation_0shots.sh
@@ -9,47 +9,37 @@ MODE="full"
 SUFFIX="_$MODE"
 
 
+    # squad_v2_ctxt_answerable \
+    # cwq \
 for eval_name in \
-    cwq \
     qa_arc_easy \
     qa_arc_challenge \
     metaqa_1hop \
     metaqa_2hop \
     metaqa_3hop \
     mintaka_multihop \
-    squad_v2_ctxt_answerable \
     qa_qasc
 do
     echo
     echo "============ $eval_name ============"
     echo "====== Evaluating Pretrained $MODEL_NAME ======"
-    model_path=gpt2
-    uv run python $PROJECT_BASE_PATH/src/evaluate.py \
-        --model $model_path \
-        --mode $MODE \
-        --tokenizer $MODEL_NAME \
-        --test-data $PROJECT_BASE_PATH/input/evaluate_data/unformated/$eval_name/test.json \
-        --score-on $SCORE_ON \
-        --sample-num $SAMPLE_NUM \
-        -o $PROJECT_BASE_PATH/output/$MODEL_NAME/HuggingFace/hf/evaluation$SUFFIX/${SCORE_ON}/${FEWSHOTS}_shots/$eval_name
+    # model_path=gpt2
+    # python $PROJECT_BASE_PATH/src/evaluate.py \
+    #     --model $model_path \
+    #     --mode $MODE \
+    #     --tokenizer $MODEL_NAME \
+    #     --test-data $PROJECT_BASE_PATH/input/evaluate_data/unformated/$eval_name/test.json \
+    #     --score-on $SCORE_ON \
+    #     --sample-num $SAMPLE_NUM \
+    #     -o $PROJECT_BASE_PATH/output/$MODEL_NAME/HuggingFace/hf/evaluation$SUFFIX/${SCORE_ON}/${FEWSHOTS}_shots/$eval_name
 
     for model_folder in \
-        random/rnd \
-        nonce/smolLM2_135M_sents_shuffled_bs1024_ep1 \
-        nonce/smolLM2_135M_sents_shuffled_bs1024_ep1-ext_test_mix_ep3 \
-        nonce/smolLM2_135M_sents_shuffled_bs1024_ep1-ext_test_mix_ep3-sft_squad_train_val_ans_ep3 \
-        nonce/smolLM2_135M_sents_shuffled_bs1024_ep1-ext_test_mix_ep3-sft_squad_test_ans_ep3 \
-        smolLM2/smolLM2_bs1024_dl0_ep1 \
-        smolLM2/smolLM2_bs1024_dl0_ep1-ext_test_mix_ep3 \
-        smolLM2/smolLM2_bs1024_dl0_ep1-ext_test_mix_ep3-sft_squad_train_val_ans_ep3 \
-        smolLM2/smolLM2_bs1024_dl0_ep1-ext_test_mix_ep3-sft_squad_test_ans_ep3 \
-        HuggingFace/hf-ext_test_mix_ep3 \
-        HuggingFace/hf-ext_test_mix_ep3-sft_squad_train_val_ans_ep3 \
-        HuggingFace/hf-ext_test_mix_ep3-sft_squad_test_ans_ep3;
+        HuggingFace/hf-ext_test_mix_ep3-sft_qa_test_wo_context \
+        nonce/smolLM2_135M_sents_shuffled_bs1024_ep1-ext_test_mix_ep3-sft_qa_test_wo_context
     do
         echo "====== Evaluating $model_folder of $MODEL_NAME ======"
         model_path=$PROJECT_BASE_PATH/output/$MODEL_NAME/$model_folder
-        uv run python $PROJECT_BASE_PATH/src/evaluate.py \
+        python $PROJECT_BASE_PATH/src/evaluate.py \
             --model $model_path \
             --mode $MODE \
             --tokenizer $MODEL_NAME \
