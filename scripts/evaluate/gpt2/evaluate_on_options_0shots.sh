4192be512e45e5aec139c66c041e38d160f54607
outenki <outenki@gmail.com>
2026-02-23 18:40:39 +0900

    update scripts

--- scripts/evaluate/gpt2/evaluate_on_options_0shots.sh
+++ scripts/evaluate/gpt2/evaluate_on_options_0shots.sh
@@ -1,29 +1,49 @@
 #!/bin/bash
-PROJECT_BASE_PATH="${PROJECT_BASE_PATH:-/home/pj25000107/ku50001566/projects/knowledge_decoupling}"
+PROJECT_BASE_PATH="${PROJECT_BASE_PATH:-$HOME/projects/knowledge_decoupling}"
 
 MODEL_NAME=gpt2
-SCORE_ON=options
+SCORE_ON=generation
 FEWSHOTS=0
 SAMPLE_NUM=1000
-MODE="simple"
-SUFFIX="_20251215"
+MODE="full"
+SUFFIX="_$MODE"
 
-for eval_name in verb_agreement fce_5gram qa_arc_easy qa_arc_challenge qa_qasc qa_boolq_ctxt; do
+
+    # squad_v2_ctxt_answerable \
+    # cwq \
+for eval_name in \
+    qa_arc_easy \
+    qa_arc_challenge \
+    metaqa_1hop \
+    metaqa_2hop \
+    metaqa_3hop \
+    mintaka_multihop \
+    qa_qasc
+do
     echo
     echo "============ $eval_name ============"
+    echo "====== Evaluating Pretrained $MODEL_NAME ======"
+    # model_path=gpt2
+    # python $PROJECT_BASE_PATH/src/evaluate.py \
+    #     --model $model_path \
+    #     --mode $MODE \
+    #     --tokenizer $MODEL_NAME \
+    #     --test-data $PROJECT_BASE_PATH/input/evaluate_data/unformated/$eval_name/test.json \
+    #     --score-on $SCORE_ON \
+    #     --sample-num $SAMPLE_NUM \
+    #     -o $PROJECT_BASE_PATH/output/$MODEL_NAME/HuggingFace/hf/evaluation$SUFFIX/${SCORE_ON}/${FEWSHOTS}_shots/$eval_name
 
     for model_folder in \
-        HuggingFace/hf-ext_test-ep3 \
-        smolLM2/smolLM2-bs1024-dl0-ep1-ext_test-ep3 \
-        nonce/smolLM2-nonce-bs1024-dl0-ep1-ext_test-ep3
+        HuggingFace/hf-ext_test_mix_ep3-sft_qa_test_wo_context \
+        nonce/smolLM2_135M_sents_shuffled_bs1024_ep1-ext_test_mix_ep3-sft_qa_test_wo_context
     do
         echo "====== Evaluating $model_folder of $MODEL_NAME ======"
         model_path=$PROJECT_BASE_PATH/output/$MODEL_NAME/$model_folder
-        /home/pj25000107/ku50001566/.local/bin/uv run python $PROJECT_BASE_PATH/src/evaluate.py \
+        python $PROJECT_BASE_PATH/src/evaluate.py \
             --model $model_path \
             --mode $MODE \
             --tokenizer $MODEL_NAME \
-            --test-data $PROJECT_BASE_PATH/input/evaluate_data/$eval_name/test.json \
+            --test-data $PROJECT_BASE_PATH/input/evaluate_data/unformated/$eval_name/test.json \
             --score-on $SCORE_ON \
             --sample-num $SAMPLE_NUM \
             -o $model_path/evaluation$SUFFIX/${SCORE_ON}/${FEWSHOTS}_shots/$eval_name
