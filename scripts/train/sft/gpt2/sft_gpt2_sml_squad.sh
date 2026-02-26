943eba39c260de2ad9f3923648bd31c9775c7bbe
outenki <outenki@gmail.com>
2026-02-26 10:54:39 +0900

    continue sft

--- scripts/train/sft/gpt2/sft_gpt2_sml_squad.sh
+++ scripts/train/sft/gpt2/sft_gpt2_sml_squad.sh
@@ -2,7 +2,7 @@
 
 PROJECT_BASE_PATH="${PROJECT_BASE_PATH:-$HOME/projects/knowledge_decoupling}"
 SCRIPT_PATH=$PROJECT_BASE_PATH/scripts/train/sft/gpt2
-INPUT_MODEL=$PROJECT_BASE_PATH/output/smolLM2/smolLM2/smolLM2_bs1024_dl0_ep1
+INPUT_MODEL=$PROJECT_BASE_PATH/output/gpt2/smolLM2/smolLM2_bs1024_dl0_ep1
 
 # /bin/bash "$SCRIPT_PATH"/sft_w_context_test.sh \
 #    "$INPUT_MODEL" \
