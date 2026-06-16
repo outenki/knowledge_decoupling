e3d98559db499509b12c462ba0e0c2ffba856ff7
outenki <outenki@gmail.com>
2026-06-16 14:30:42 +0900

    add training script

---
+++ scripts/_jobs/train/hkd/train_sml2_135M_sml_job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#PBS -q sg
+#PBS -l select=1:ngpus=4
+#PBS -l walltime=100:00:00
+#PBS -W group_list=c30897
+#PBS -j oe
+#PBS -o logs/train_smolLM2_135M_sml.log
+
+
+source $HOME/.zshrc
+cd $PROJECT_BASE_PATH/src/train
+
+uv run python train.py --config-name SmolLM2-135M-sml base.path=$PROJECT_BASE_PATH
