cbb60a9e87b1384dd395abe1452b869f8bd1c00b
outenki <outenki@gmail.com>
2026-06-17 14:08:17 +0900

    update training scripts for sml2_135M

--- scripts/_jobs/train/hkd/train_sml2_135M_nonce_job.sh
+++ scripts/_jobs/train/hkd/train_sml2_135M_nonce_job.sh
@@ -10,5 +10,5 @@
 source $HOME/.zshrc
 cd $PROJECT_BASE_PATH/src/train
 
-export UV_OFFLINE=1
-uv run python train.py --config-name SmolLM2-135M-sml base.path=$PROJECT_BASE_PATH
+# export UV_OFFLINE=1
+uv run python train.py --config-name SmolLM2-135M-nonce base.path=$PROJECT_BASE_PATH
