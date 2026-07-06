f5e6208c63ba7bf441e591137a57d248cb3d46d0
outenki <outenki@gmail.com>
2026-07-06 13:38:40 +0900

    update training scripts and config

--- scripts/_jobs/train/hkd/train_llama3.2_1B_core_job.sh
+++ scripts/_jobs/train/hkd/train_llama3.2_1B_core_job.sh
@@ -12,4 +12,4 @@ cd $PROJECT_BASE_PATH/src/train
 
 # export UV_OFFLINE=1
 export WANDB_MODE=offline
-uv run python train.py --config-name Llama3.2_1B_core base.path=$PROJECT_BASE_PATH
+uv run python train.py --config-name Llama3.2_1B-core base.path=$PROJECT_BASE_PATH
