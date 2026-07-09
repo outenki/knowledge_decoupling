5d35ae48b139c17f9dbed59c8d57d0fb07c14660
outenki <outenki@gmail.com>
2026-07-09 16:16:17 +0900

    update train scripts

---
+++ scripts/_jobs/train/hkd/train_olmo2_1B_core_job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#PBS -q sg
+#PBS -l select=1:ngpus=4
+#PBS -l walltime=150:00:00
+#PBS -W group_list=c30897
+#PBS -j oe
+#PBS -o logs/train_olmo2_1B_core.log
+
+
+source $HOME/.zshrc
+cd $PROJECT_BASE_PATH/src/train
+
+# export UV_OFFLINE=1
+export WANDB_MODE=offline
+uv run python train.py --config-name olmo2_1B-core base.path=$PROJECT_BASE_PATH
---
+++ scripts/_jobs/train/hkd/train_qwen2.5_0.5B_sml_job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#PBS -q sg
+#PBS -l select=1:ngpus=4
+#PBS -l walltime=150:00:00
+#PBS -W group_list=c30897
+#PBS -j oe
+#PBS -o logs/train_qwen2.5_0.5B_sml.log
+
+
+source $HOME/.zshrc
+cd $PROJECT_BASE_PATH/src/train
+
+# export UV_OFFLINE=1
+export WANDB_MODE=offline
+uv run python train.py --config-name qwen2.5_0.5B-sml base.path=$PROJECT_BASE_PATH
