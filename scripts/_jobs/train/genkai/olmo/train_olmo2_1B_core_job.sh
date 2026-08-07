8d12e2f6e0b6c43f58e99e379d4c7af714f218e8
outenki <outenki@gmail.com>
2026-08-07 16:23:12 +0900

    update train scripts for genkai

---
+++ scripts/_jobs/train/genkai/olmo/train_olmo2_1B_core_job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#PJM -L "rscgrp=b-batch"
+#PJM -L "elapse=150:00:00"
+#PJM -L "gpu=4"
+#PJM -e logs/train_olmo2_1B_core.log
+#PJM -o logs/train_olmo2_1B_core.log
+#PJM -N "tr_olmo_core"
+
+
+source $HOME/.zshrc
+cd $PROJECT_BASE_PATH/src/train
+
+# export UV_OFFLINE=1
+export WANDB_MODE=offline
+uv run python train.py --config-name olmo2_1B-core base.path=$PROJECT_BASE_PATH
---
+++ scripts/_jobs/train/genkai/qwen2.5/train_qwen2.5_0.5B_core_job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#PJM -L "rscgrp=b-batch"
+#PJM -L "elapse=150:00:00"
+#PJM -L "gpu=4"
+#PJM -e logs/train_qwen2.5_0.5B_core.log
+#PJM -o logs/train_qwen2.5_0.5B_core.log
+#PJM -N "tr_qwen_core"
+
+
+source $HOME/.zshrc
+cd $PROJECT_BASE_PATH/src/train
+
+# export UV_OFFLINE=1
+export WANDB_MODE=offline
+uv run python train.py --config-name qwen2.5_0.5B-core base.path=$PROJECT_BASE_PATH
